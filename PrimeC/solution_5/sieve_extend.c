e73a36f0bda72f9853b3097f0ec5cf10c73d6bc4
Rogier van Dam <rogiervandam@gmail.com>
2022-10-28 11:43:26 +0200

    shiftright unrolled improvement

--- PrimeC/solution_5/sieve_extend.c
+++ PrimeC/solution_5/sieve_extend.c
@@ -367,10 +367,10 @@ static void extendSieve_shiftright(bitword_t* bitarray, const counter_t source_s
 
     const counter_t destination_stop_word = wordindex(destination_stop);
     const counter_t copy_start = source_start + size;
-    const bitshift_t shift = bitindex_calc(copy_start) - bitindex_calc(source_start);
-    const bitshift_t shift_flipped = WORD_SIZE_bitshift-shift;
-    counter_t source_word = wordindex(source_start);
-    counter_t copy_word = wordindex(copy_start);
+    register const bitshift_t shift = bitindex_calc(copy_start) - bitindex_calc(source_start);
+    register const bitshift_t shift_flipped = WORD_SIZE_bitshift-shift;
+    register counter_t source_word = wordindex(source_start);
+    register counter_t copy_word = wordindex(copy_start);
     counter_t source_lastword = wordindex(copy_start);
     bitarray[copy_word] |= ((bitarray[source_word] << shift)  // or the start in to not lose data
                                 | (bitarray[source_lastword] >> shift_flipped))
@@ -379,21 +379,49 @@ static void extendSieve_shiftright(bitword_t* bitarray, const counter_t source_s
     source_word++;
 
     const counter_t aligned_copy_word = min(source_word + size, destination_stop_word); // after <<size>> words, just copy at word level
-    const counter_t fast_loop_stop_word = (aligned_copy_word>2) ? (aligned_copy_word - 2) : 0; // safe for unsigned ints
 
-    debug printf("...start - %ju - end fastloop - %ju - alignment - %ju - end\n", (uintmax_t)fast_loop_stop_word - (uintmax_t)wordindex(copy_start), (uintmax_t)aligned_copy_word - (uintmax_t)fast_loop_stop_word, (uintmax_t)destination_stop_word - (uintmax_t)aligned_copy_word);
     
-    while (copy_word < fast_loop_stop_word) {
+    if (copy_word - source_word > 8) {
+        const counter_t fast_loop_stop_word = (aligned_copy_word>8) ? (aligned_copy_word - 8) : 0;
+        debug printf("...start - %ju - end fastloop - %ju - alignment - %ju - end - unrolled\n", (uintmax_t)fast_loop_stop_word - (uintmax_t)wordindex(copy_start), (uintmax_t)aligned_copy_word - (uintmax_t)fast_loop_stop_word, (uintmax_t)destination_stop_word - (uintmax_t)aligned_copy_word);
+        while (copy_word < fast_loop_stop_word) {
             bitword_t sourcen = bitarray[source_word-1];
             bitword_t source0 = bitarray[source_word  ];
+            bitword_t source1 = bitarray[source_word+1];
+            bitword_t source2 = bitarray[source_word+2];
+            bitword_t source3 = bitarray[source_word+3];
+            bitword_t source4 = bitarray[source_word+4];
+            bitword_t source5 = bitarray[source_word+5];
+            bitword_t source6 = bitarray[source_word+6];
+            bitword_t source7 = bitarray[source_word+7];
 
+            bitarray[copy_word  ] = (source0 << shift) | (sourcen >> shift_flipped);
+            bitarray[copy_word+1] = (source1 << shift) | (source0 >> shift_flipped);
+            bitarray[copy_word+2] = (source2 << shift) | (source1 >> shift_flipped);
+            bitarray[copy_word+3] = (source3 << shift) | (source2 >> shift_flipped);
+            bitarray[copy_word+4] = (source4 << shift) | (source3 >> shift_flipped);
+            bitarray[copy_word+5] = (source5 << shift) | (source4 >> shift_flipped);
+            bitarray[copy_word+6] = (source6 << shift) | (source5 >> shift_flipped);
+            bitarray[copy_word+7] = (source7 << shift) | (source6 >> shift_flipped);
+            copy_word += 8;
+            source_word += 8;
+        }
+    }
+    else {
+        const counter_t fast_loop_stop_word = (aligned_copy_word>2) ? (aligned_copy_word - 2) : 0; // safe for unsigned ints
+        debug printf("...start - %ju - end fastloop - %ju - alignment - %ju - end\n", (uintmax_t)fast_loop_stop_word - (uintmax_t)wordindex(copy_start), (uintmax_t)aligned_copy_word - (uintmax_t)fast_loop_stop_word, (uintmax_t)destination_stop_word - (uintmax_t)aligned_copy_word);
+        while (copy_word < fast_loop_stop_word) {
+            bitword_t sourcen = bitarray[source_word-1];
+            register bitword_t source0 = bitarray[source_word  ];
+            
             bitarray[copy_word  ] = (source0 << shift) | (sourcen >> shift_flipped);
             bitword_t source1 = bitarray[source_word+1];
             bitarray[copy_word+1] = (source1 << shift) | (source0 >> shift_flipped);
             copy_word += 2;
             source_word += 2;
+        }
     }
-
+    
     while (copy_word <= aligned_copy_word) {
         bitarray[copy_word] = (bitarray[source_word-1] >> shift_flipped) | (bitarray[source_word] << shift);
         copy_word++;
