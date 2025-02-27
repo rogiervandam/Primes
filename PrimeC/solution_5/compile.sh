f8fdd0064d2850f79b39a0f28ddb46c0586aad6f
Rogier van Dam <rogiervandam@gmail.com>
2025-02-27 11:11:16 +0100

    removed binaries

--- PrimeC/solution_5/compile.sh
+++ PrimeC/solution_5/compile.sh
@@ -3,9 +3,8 @@ CC="gcc -Ofast -march=native -mtune=native -fno-asynchronous-unwind-tables -fno-
 PAR="-fopenmp"
 PAREXT="_epar"
 
-# -o sieve_extend-u64_v4 sieve_extend.c -Du64_v4 -s
 echo "Compiling..."
-$CC -Q --help=target
+# $CC -Q --help=target
 for x in sieve_base sieve_extend; do
     for y in u32_v8 u64_v4 u64_v8 u64_v2; do
         $CC -o $x-$y $x.c -D$y -s
