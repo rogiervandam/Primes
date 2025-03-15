829032a5e8639655e1516031e1bac1c36f172287
Rogier van Dam <rogiervandam@gmail.com>
2025-03-15 10:26:36 +0100

    changed defaults for intel

--- PrimeC/solution_5/src/general/settings.h
+++ PrimeC/solution_5/src/general/settings.h
@@ -3,33 +3,31 @@
     #define COMPILE_VERBOSE_LEVEL 2  // Set to 1-9 to enable compiling different verbose levels
 #endif
 
-#define cache_line_bytes   256 // How to align the caches
+
 // #define COMPILE_CHECKALL                 
+
 // #define COMPILE_EXPLAIN                  
 
 
+// How to align the caches
+#define cache_line_bytes      256 
+
+
+// type for handling non-vector bitwise operations in the bitstorage
 #ifndef bitword_t
-    #ifdef __APPLE__
-        #define bitword_t  uint32_t // type used to store bits
-        #define BITWORD_T_SIZE_PP 32
-    #else
-        #define bitword_t  uint64_t // type used to store bits
-        #define BITWORD_T_SIZE_PP 64
-    #endif
+    #define bitword_t         uint32_t 
+    #define BITWORD_T_SIZE_PP 32
 #endif
 
+// type for describing the index of a bit in the sieve and general loops
 #ifndef counter_t
-    #ifdef __APPLE__
-        #define counter_t int32_t     // apple m1 really likes 32 bit int for counters and not 64
-        #define COUNTER_T_SIZE_PP 32
-    #else
-        #define counter_t  int64_t  // type used to count loops, etc. Some processors/compilers are faster at 32 bits
-        #define COUNTER_T_SIZE_PP 64
-    #endif
+    #define counter_t         int32_t
+    #define COUNTER_T_SIZE_PP 32
 #endif
 
+// type used to shift bits
 #ifndef bitshift_t
-    #define bitshift_t counter_t // type used to shift bits
+    #define bitshift_t counter_t 
 #endif
 
 
