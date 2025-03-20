5d2e95dc2dc21acd6123dc7f8670fd0e6b2a279e
Rogier van Dam <rogiervandam@gmail.com>
2025-03-20 10:59:02 +0100

    after initial cleanup

--- PrimeC/solution_5/src/bitstorage/bitstorage_applyMask_vector.h
+++ PrimeC/solution_5/src/bitstorage/bitstorage_applyMask_vector.h
@@ -1,70 +1,22 @@
+// make the general applyMask_vector function (TODO: remove later)
 #define variantsuffix _vector
 #define bitbucket_t bitvector_t
-// #undef unrolls
 #include "bitstorage_applyMask.h"
 
+// make the versions for the different vector sizes
 #define variant uint64v2
 #include "bitstorage_applyMask.h"
-
 #define variant uint64v4
 #include "bitstorage_applyMask.h"
-
 #define variant uint64v8
 #include "bitstorage_applyMask.h"
-
 #define variant uint16v8
 #include "bitstorage_applyMask.h"
-
 #define variant uint16v32
 #include "bitstorage_applyMask.h"
-
 #define variant uint32v8
 #include "bitstorage_applyMask.h"
-
 #define variant uint32v16
 #include "bitstorage_applyMask.h"
 
-static inline void __attribute__((always_inline)) applyMask_vector_pair(bitvector_t* restrict bitstorage, const counter_t step, const counter_t range_stop, const bitvector_t mask1, const bitvector_t mask2, counter_t index_vector) 
-{
-    verbose8( printf("Applying mask with step %ju in range until %ju", (uintmax_t)step, (uintmax_t)range_stop); )
-    timer_lapstart(time_applyMask_vector);
-
-    const counter_t range_stop_vector = vectorindex(range_stop);
-   
-    register const counter_t step_4 = step << 2;
-
-    register bitvector_t* restrict index_ptr      =  __builtin_assume_aligned(&bitstorage[index_vector],sizeof(bitvector_t));
-    #if is_signed(counter_t)
-    register bitvector_t* restrict fast_loop_ptr  =  __builtin_assume_aligned(&bitstorage[range_stop_vector] - step_4,sizeof(bitvector_t));
-    #else
-    register bitvector_t* restrict fast_loop_ptr  =  __builtin_assume_aligned(&bitstorage[((range_stop_vector > step_4) ? (range_stop_vector - step_4):0)],sizeof(bitvector_t));
-    #endif
-
-    register const counter_t step_2 = step << 1;
-    register const counter_t step_3 = step_2 + step;
-    
-    #pragma GCC ivdep
-    while likely(index_ptr < fast_loop_ptr) {
-        *index_ptr                |= mask1;
-        *(index_ptr + 1)          |= mask2;  
-        *(index_ptr + step  )     |= mask1; 
-        *(index_ptr + step + 1)   |= mask2;  
-        *(index_ptr + step_2)     |= mask1; 
-        *(index_ptr + step_2 + 1) |= mask2;  
-        *(index_ptr + step_3)     |= mask1; 
-        *(index_ptr + step_3 + 1) |= mask2;  
-        index_ptr += step_4;
-    }
-    
-    register const bitvector_t* restrict range_stop_ptr = __builtin_assume_aligned(&bitstorage[(range_stop_vector)],sizeof(bitvector_t));
-    
-    for (counter_t i=5; i-- && likely(index_ptr < range_stop_ptr); index_ptr += step) { // signal compiler that only <4 iterations are left
-        *index_ptr     |= mask1; 
-        *(index_ptr+1) |= mask2; 
-    }
-    
-    if (index_ptr == range_stop_ptr) {
-        *index_ptr     |= mask1; 
-    }
-    timer_laptime(time_applyMask_vector); verbose8( printf("\n"); )
-}
+#include "bitstorage_applyMask_pair.h"
