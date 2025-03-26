1de08303ad62a40e679c64ff81191e3960074f70
Rogier van Dam <rogiervandam@gmail.com>
2025-03-26 11:07:24 +0100

    dockerfile rework

--- PrimeC/solution_5/tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# gcc -o tests/bin/test_sieve_create tests/src/test_sieve_create.c
-# tests/bin/test_sieve_create
-
-gcc -o tests/bin/test_sieve_continue_pattern tests/src/test_sieve_continue_pattern.c
-tests/bin/test_sieve_continue_pattern
-
-# valgrind --leak-check=full tests/bin/test_sieve_create
