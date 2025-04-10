b9df25af1701353a229253848065c6571279f927
Rogier van Dam <rogiervandam@gmail.com>
2025-04-10 17:03:50 +0200

    support forcing compiler

--- PrimeC/solution_5/src/benchmark/sieve_options.h
+++ PrimeC/solution_5/src/benchmark/sieve_options.h
@@ -51,8 +51,8 @@ setDefaultOptions()
 
     option.check                      = 1; // set to 2 to stop after the check algorithm
     option.tunelevel                  = 1;
-    option.initial_sample_duration    = 0.0005;
-    option.next_sample_duration       = 0.002;
+    option.initial_sample_duration    = 0.001;
+    option.next_sample_duration       = 0.003;
     option.warmup_duration            = 1;
     option.tune_duration_max          = 10.0;
     option.tune_keeppercent_longlist  = 10;
