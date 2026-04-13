cc4d1ad9d66582545691469104fab38a3fe0a5a9
Rogier van Dam <rogiervandam@gmail.com>
2026-04-13 09:51:14 +0200

    refactor applymask

--- PrimeC/solution_5/src/bitstorage/bitstorage_setBitsTrue_applyMask.h
+++ PrimeC/solution_5/src/bitstorage/bitstorage_setBitsTrue_applyMask.h
@@ -6,87 +6,90 @@ function(applyMask_index,suffix)(void* restrict bitstorage, const counter_t rang
 {
     startAnalysis8(time_applyMask, "\nApplying %s mask with step %ju in range until %ju", STR(bitbucket_t), (uintmax_t)step, (uintmax_t)range_stop);
   
-    register const counter_t step_max                   = step * unrolls;
-    register bitbucket_t* restrict bitstorage_sized     = __builtin_assume_aligned(bitstorage, cache_line_bytes);
-    register bitbucket_t* restrict index_ptr            = __builtin_assume_aligned(&bitstorage_sized[range_start],sizeof(bitbucket_t));
-    register const bitbucket_t* restrict range_stop_ptr = __builtin_assume_aligned(&bitstorage_sized[range_stop],sizeof(bitbucket_t));
-    register counter_t i = safe_diff(range_stop, range_start) / step;
+    register       bitbucket_t* restrict bitstorage_sized   = __builtin_assume_aligned(bitstorage, cache_line_bytes);
+    register       bitbucket_t* restrict index_ptr          = __builtin_assume_aligned(&bitstorage_sized[range_start],sizeof(bitbucket_t));
+    register const bitbucket_t* restrict range_stop_ptr     = __builtin_assume_aligned(&bitstorage_sized[range_stop],sizeof(bitbucket_t));
 
     #if defined(__GNUC__) && !defined(__clang__) // optimized for GCC
-    register const bitbucket_t* restrict fast_loop_ptr  = __builtin_assume_aligned(&bitstorage_sized[safe_diff(range_stop,step_max)],sizeof(bitbucket_t));
+        register const counter_t step_max                   = step * unrolls;
+        register const bitbucket_t* restrict fast_loop_ptr  = __builtin_assume_aligned(&bitstorage_sized[safe_diff(range_stop,step_max)],sizeof(bitbucket_t));
 
-    #if unrolls == 16
-    #pragma GCC ivdep
-    #pragma GCC unroll 32
-    for(;(index_ptr < fast_loop_ptr); ) {
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-    }
-    #endif
+        #if unrolls == 16
+        #pragma GCC ivdep
+        #pragma GCC unroll 32
+        for(;(index_ptr < fast_loop_ptr); ) {
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+        }
+        #endif
 
-    #if unrolls == 8
-    #pragma GCC ivdep
-    #pragma GCC unroll 64
-    for(;(index_ptr < fast_loop_ptr); ) {
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-    }
-    #endif
+        #if unrolls == 8
+        #pragma GCC ivdep
+        #pragma GCC unroll 64
+        for(;(index_ptr < fast_loop_ptr); ) {
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+        }
+        #endif
 
-    #if unrolls == 4
-    #pragma GCC ivdep
-    #pragma GCC unroll 64
-    for(;(index_ptr < fast_loop_ptr); ) {
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-        *index_ptr |= mask; index_ptr += step;
-    }
-    #endif
+        #if unrolls == 4
+        #pragma GCC ivdep
+        #pragma GCC unroll 64
+        for(;(index_ptr < fast_loop_ptr); ) {
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+            *index_ptr |= mask; index_ptr += step;
+        }
+        #endif
 
     #elif defined(__clang__) // optimized for clang
 
-    #if unrolls >= 8
-        for (;i>8; i-=8, index_ptr += step * 8) {
-            index_ptr[step * 0] |= mask;
-            index_ptr[step * 1] |= mask;
-            index_ptr[step * 2] |= mask;
-            index_ptr[step * 3] |= mask;
-            index_ptr[step * 4] |= mask;
-            index_ptr[step * 5] |= mask;
-            index_ptr[step * 6] |= mask;
-            index_ptr[step * 7] |= mask;
-        }
-    #elif unrolls >= 4
-        for (;i>4; i-=4, index_ptr += step * 4) {
-            index_ptr[step * 0] |= mask;
-            index_ptr[step * 1] |= mask;
-            index_ptr[step * 2] |= mask;
-            index_ptr[step * 3] |= mask;
-        }
-    #endif
+        register counter_t i = safe_diff(range_stop, range_start) / step;
 
-    #endif
+        #if unrolls >= 8
+            register const counter_t step_8 = step * 8;
+            for (; i > 8 ; i -= 8, index_ptr += step_8) {
+                index_ptr[step * 0] |= mask;
+                index_ptr[step * 1] |= mask;
+                index_ptr[step * 2] |= mask;
+                index_ptr[step * 3] |= mask;
+                index_ptr[step * 4] |= mask;
+                index_ptr[step * 5] |= mask;
+                index_ptr[step * 6] |= mask;
+                index_ptr[step * 7] |= mask;
+            }
+        #elif unrolls >= 4
+            register const counter_t step_4 = step * 4;
+            for (; i > 4 ; i -= 4, index_ptr += step_4) {
+                index_ptr[step * 0] |= mask;
+                index_ptr[step * 1] |= mask;
+                index_ptr[step * 2] |= mask;
+                index_ptr[step * 3] |= mask;
+            }
+        #endif
+
+    #endif // end of clang section
 
     for (; likely(index_ptr <= range_stop_ptr); index_ptr += step) { // signal compiler that only < unrolls iterations are left
         *index_ptr |= mask; 
