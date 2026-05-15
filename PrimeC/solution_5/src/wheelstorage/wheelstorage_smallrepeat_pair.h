67172276880d455d6d6bafea804f93e9acbc8873
Rogier van Dam <rogiervandam@gmail.com>
2026-05-15 16:10:44 +0200

    refactoring smallrepeat_pair

--- PrimeC/solution_5/src/wheelstorage/wheelstorage_smallrepeat_pair.h
+++ PrimeC/solution_5/src/wheelstorage/wheelstorage_smallrepeat_pair.h
@@ -16,18 +16,15 @@ function(markFactors_wheelstorage_small_repeat_pair,suffix)(sieve_t* sieve, coun
 
     const counter_t stop_number_unique = min(getFactor( bitbucket_start_type(wheel_bit_estimate_last(start_number), bitbucket_t) - 1 + wheel_step * wheelmask_stripe_bits * ((bitcount_type(bitbucket_t) + wheelmask_stripe_bits - 1) / wheelmask_stripe_bits)), stop_number);
     bitbucket_t current_mask = (bitbucket_t)0U, pending_mask = (bitbucket_t)0U;
-    counter_t pending_bucket = 0, current_bucket = 0;
+    counter_t pending_bucket = 0, current_bucket = 0; // initialize current_bucket to 0 always triggers pending mask, but meybe cheaper than calculating the first bucket beforehand
 
     log8(time_markFactors_wheelstorage_small_repeat_pair, "Marking numbers for prime %ju with step %ju using 'repeating pairs' of %s in %ju number range (%ju-%ju) starting at number %ju", 
         (uintmax_t)step/2, (uintmax_t)step, STR(suffix), (uintmax_t)safe_diff(stop_number,start_number),(uintmax_t)start_number,(uintmax_t)stop_number, (uintmax_t)((safe_diff(stop_number,start_number))/(uintmax_t)step), 
         (uintmax_t)(((uintmax_t)safe_diff(stop_number,start_number))/(uintmax_t)(bitcount_type(bitbucket_t)*step)) );
 
     for (counter_t index = start_number; index <= stop_number_unique; index += step) {
-        const counter_t wheel_bit = wheel_bit_calc(index);
+
         const counter_t new_bucket = function(wheel_bucket_calc,variant_suffix)(index);
-
-        if (wheel_bit < 0) continue; // if the number is divisible by any of the wheel primes, skip it
-
         if (new_bucket != current_bucket) {
             if (pending_mask) {
                 if (current_mask && ((pending_bucket + 1) == current_bucket )) { 
@@ -45,7 +42,8 @@ function(markFactors_wheelstorage_small_repeat_pair,suffix)(sieve_t* sieve, coun
             current_mask = (bitbucket_t)0U;
         }
 
-        current_mask |= markmask_type(wheel_bit, bitbucket_t);
+        const counter_t wheel_bit = wheel_bit_calc(index);
+        if (wheel_bit >= 0) current_mask |= markmask_type(wheel_bit, bitbucket_t);
     }
 
     if (pending_mask) {
