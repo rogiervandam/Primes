e147a1422abff8d47a9dfd62cb99359fd93d4623
Rogier van Dam <rogiervandam@gmail.com>
2026-06-03 23:21:10 +0200

    fastest on i8700

--- PrimeC/solution_5/src/wheelstorage/wheelstorage_markFactors.h
+++ PrimeC/solution_5/src/wheelstorage/wheelstorage_markFactors.h
@@ -15,12 +15,12 @@ markFactors_wheelstorage(void* restrict bitstorage, counter_t start, counter_t s
     // else
     if (step < global_largestep_faster) {
     // if (step < 256) {
-        // markFactors_wheelstorage_small_repeat_mmask2_uint64_unroll8(bitstorage, start, stop, step);
+        markFactors_wheelstorage_small_repeat_mmask2_uint64_unroll8(bitstorage, start, stop, step);
         // markFactors_wheelstorage_small_repeat_uint64_unroll8(bitstorage, start, stop, step);
         // markFactors_wheelstorage_repeat_uint8_unroll8(bitstorage, start, stop, step);
         // markFactors_wheelstorage_repeat_uint64_unroll8(bitstorage, start, stop, step);
         // markFactors_wheelstorage_small_repeat_pair_uint64_unroll8(bitstorage, start, stop, step);
-        markFactors_wheelstorage_small_repeat_pair_uint64_unroll8(bitstorage, start, stop, step);
+        // markFactors_wheelstorage_small_repeat_pair_uint64_unroll8(bitstorage, start, stop, step);
         // markFactors_wheelstorage_small_repeat_pair_vector_uint64v4_unroll8(bitstorage, start, stop, step);
     }
     else 
